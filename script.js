0ae8888ed464f78f1407e09ef808bad062edda14
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2022-10-02 14:56:22 +0100

    condensed playRound function

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let playerScore = 0;
 let computerScore = 0;
 
+let possibleChoices = ['rock', 'paper', 'scissors'];
+
 const scoreboard = document.querySelector('#scoreboard');
 let playerScoreMessage = document.querySelector('#player-score-message');
 let computerScoreMessage = document.querySelector('#computer-score-message');
@@ -14,25 +16,20 @@ const gameOverScreen = document.querySelector('#game-over-screen')
 const finalResult = document.querySelector('#final-result')
 
 rockButton.addEventListener('click', function() {
-    choiceClicked('rock')
+    choiceClicked(possibleChoices[0])
 });
-
 paperButton.addEventListener('click', function() {
-    choiceClicked('paper')
+    choiceClicked(possibleChoices[1])
 });
-
 scissorsButton.addEventListener('click', function() {
-    choiceClicked('scissors')
+    choiceClicked(possibleChoices[2])
 });
 
 function choiceClicked(selection) {
     let playerSelection = selection;
-    console.log(`${playerSelection} clicked`)
     let computerSelection = getComputerChoice();
     playRound(playerSelection, computerSelection);
 }
-
-let possibleChoices = ['rock', 'paper', 'scissors'];
 
 function getComputerChoice() {
     let computerChoice = possibleChoices[Math.floor(Math.random() * 3)];
@@ -40,45 +37,36 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    console.log(playerSelection);
-    console.log(computerSelection);
     if (playerSelection === computerSelection) {
-        roundResult = "draw";
-        console.log(roundResult)
-        roundResultMessage(roundResult, playerSelection, computerSelection);
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        roundResult = "win";
-        playerScore++;
-        updateScores(playerScore, computerScore)
-        roundResultMessage(roundResult, playerSelection, computerSelection);
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-        roundResult = "lose";
-        computerScore++;
-        updateScores(playerScore, computerScore)
-        roundResultMessage(roundResult, playerSelection, computerSelection);
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        roundResult = "win";
-        playerScore++;
-        updateScores(playerScore, computerScore)
-        roundResultMessage(roundResult, playerSelection, computerSelection);
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        roundResult = "lose";
-        computerScore++;
-        updateScores(playerScore, computerScore)
-        roundResultMessage(roundResult, playerSelection, computerSelection);
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        roundResult = "win";
-        playerScore++;
-        updateScores(playerScore, computerScore)
-        roundResultMessage(roundResult, playerSelection, computerSelection);
-    } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        roundResult = "lose";
-        computerScore++;
-        updateScores(playerScore, computerScore)
-        roundResultMessage(roundResult, playerSelection, computerSelection);
-    } else {
-        console.log("error");}
-}
+        roundResult = 'draw';
+    } else if (playerSelection === possibleChoices[0]) {
+        if (computerSelection === possibleChoices[1]) {
+            roundResult = 'lose';
+            computerScore++;
+        } else {
+            roundResult = 'win';
+            playerScore++;
+        };
+    } else if (playerSelection === possibleChoices[1]) {
+        if (computerSelection === possibleChoices[2]) {
+            roundResult = 'lose';
+            computerScore++;
+        } else {
+            roundResult = 'win';
+            playerScore++;
+        };
+    } else if (playerSelection === possibleChoices[2]) {
+        if (computerSelection === possibleChoices[0]) {
+            roundResult = 'lose';
+            computerScore++;
+        } else {
+            roundResult = 'win';
+            playerScore++;
+        };
+    };
+    updateScores(playerScore, computerScore);
+    roundResultMessage(roundResult, playerSelection, computerSelection);
+};
 
 function updateScores(playerScore, computerScore) {
     playerScoreMessage.textContent = `Player Score = ${playerScore}`;
@@ -90,29 +78,29 @@ function updateScores(playerScore, computerScore) {
 
 function roundResultMessage(roundResult, playerSelection, computerSelection) {
     let declaredRoundResult = roundResult;
-    if (declaredRoundResult = "win") {
+    if (declaredRoundResult === 'win') {
         let capitalisedPlayerSelection = playerSelection[0].toUpperCase() + playerSelection.substring(1);
-        lastRoundResult.textContent = `You ${roundResult}! ${capitalisedPlayerSelection} beats ${computerSelection}.`
-    } else if (declaredRoundResult = "lose") {
+        lastRoundResult.textContent = `You ${roundResult}! ${capitalisedPlayerSelection} beats ${computerSelection}.`;
+    } else if (declaredRoundResult === 'lose') {
         let capitalisedComputerSelection = computerSelection[0].toUpperCase() + computerSelection.substring(1);
-        lastRoundResult.textContent = `You ${roundResult}! ${capitalisedComputerSelection} beats ${playerSelection}.`
-    } else if (declaredRoundResult = "draw") {
-        lastRoundResult.textContent = `It's a ${roundResult}! Both players played ${computerSelection}.`
+        lastRoundResult.textContent = `You ${roundResult}! ${capitalisedComputerSelection} beats ${playerSelection}.`;
+    } else if (declaredRoundResult === 'draw') {
+        lastRoundResult.textContent = `It's a ${roundResult}! Both players played ${computerSelection}.`;
     } else  {
-        lastRoundResult.textContent = "Error!"
+        lastRoundResult.textContent = 'Error!';
     };
-}
+};
 
 function gameOver(playerScore, computerScore) {
     rockButton.disabled = true;
     scissorsButton.disabled = true;
     paperButton.disabled = true;
-    let winningPlayer
+    let winningPlayer = '';
     if (playerScore > computerScore) {
         winningPlayer = 'Player';
     } else {
         winningPlayer = 'Computer';
-    }
+    };
     gameOverScreen.classList.remove('invisible');
     finalResult.textContent = `Final Result: ${winningPlayer} wins!`;
-}
+};
